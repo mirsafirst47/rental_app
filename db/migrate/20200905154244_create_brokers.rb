71a7bcb10abae0bf16338a2d9cf6bd4ca3219651
mirsafirst47 <habib.triande@gmail.com>
2020-09-05 16:24:40 -0400

    Fix migrations

--- db/migrate/20200905154244_create_brokers.rb
+++ db/migrate/20200905154244_create_brokers.rb
@@ -1,4 +1,4 @@
-class Broker < ActiveRecord::Migration[6.0]
+class CreateBrokers < ActiveRecord::Migration[6.0]
   def change
     create_table :brokers do |t|
       t.string :name
--- db/migrate/20200905163015_add_broker_id_to_apartments.rb
+++
@@ -1,5 +0,0 @@
-class AddBrokerIdToApartments < ActiveRecord::Migration[6.0]
-  def change
-    add_column :apartments, :broker_id, :integer
-  end
-end
